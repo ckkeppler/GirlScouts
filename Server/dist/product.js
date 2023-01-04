e54a4ed437c761a1fcbe1a8b8b7bde792fa96eb2
Chris Keppler <ckkeppler@gmail.com>
2023-01-04 15:29:59 -0500

    add server.ts

---
+++ Server/dist/product.js
@@ -0,0 +1,77 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.router = void 0;
+const express_1 = require("express");
+const model_1 = require("./model");
+exports.router = (0, express_1.Router)();
+exports.router.get("/product", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const repository = yield (0, model_1.getProductRepository)();
+        const allProducts = yield repository.find();
+        res.send(allProducts);
+    }
+    catch (err) {
+        return next(err);
+    }
+}));
+exports.router.get("/product/:id", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const repository = yield (0, model_1.getProductRepository)();
+        const product = yield repository.findOne(req.params.id);
+        res.send(product);
+    }
+    catch (err) {
+        return next(err);
+    }
+}));
+exports.router.post("/product", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const repository = yield (0, model_1.getProductRepository)();
+        const product = new model_1.Product();
+        product.name = req.body.name;
+        product.sku = req.body.sku;
+        product.description = req.body.description;
+        product.price = Number.parseFloat(req.body.price);
+        product.stock = Number.parseInt(req.body.stock);
+        const result = yield repository.save(product);
+        res.send(result);
+    }
+    catch (err) {
+        return next(err);
+    }
+}));
+exports.router.post("/product/:id", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const repository = yield (0, model_1.getProductRepository)();
+        const product = yield repository.findOne(req.params.id);
+        product.name = req.body.name;
+        product.sku = req.body.sku;
+        product.description = req.body.description;
+        product.price = Number.parseFloat(req.body.price);
+        product.stock = Number.parseInt(req.body.stock);
+        const result = yield repository.save(product);
+        res.send(result);
+    }
+    catch (err) {
+        return next(err);
+    }
+}));
+exports.router.delete("/product/:id", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const repository = yield (0, model_1.getProductRepository)();
+        yield repository.delete(req.params.id);
+        res.send("OK");
+    }
+    catch (err) {
+        return next(err);
+    }
+}));
